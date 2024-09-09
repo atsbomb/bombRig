af5723de1a755973f25d5aac952faf7f6fb74f5b
atsbomb <atsbomb@gmail.com>
2024-09-09 15:53:16 +0800

    makeIdentity for generated ik joints
    required for it to keep it in single RPlane.

--- ik.py
+++ ik.py
@@ -9,9 +9,17 @@ all = sels.pop(0)
 parent = sels.pop(0)
 
 cmds.select(cl=1)
-sj = cmds.joint(p=cmds.xform(sels[0], q=1, ws=1, t=1), n=f'{sels[0]}_ik_jnt')
-mj = cmds.joint(p=cmds.xform(sels[1], q=1, ws=1, t=1), n=f'{sels[1]}_ik_jnt')
-ej = cmds.joint(p=cmds.xform(sels[2], q=1, ws=1, t=1), n=f'{sels[2]}_ik_jnt')
+sj = cmds.joint(n=f'{sels[0]}_ik_jnt')
+cmds.matchTransform(sj, sels[0], position=1, rotation=1)
+cmds.makeIdentity(sj, apply=1, translate=1, rotate=1, scale=1)
+cmds.select(sj)
+mj = cmds.joint(n=f'{sels[1]}_ik_jnt')
+cmds.matchTransform(mj, sels[1], position=1, rotation=1)
+cmds.makeIdentity(mj, apply=1, translate=1, rotate=1, scale=1)
+cmds.select(mj)
+ej = cmds.joint(n=f'{sels[2]}_ik_jnt')
+cmds.matchTransform(ej, sels[2], position=1, rotation=1)
+cmds.makeIdentity(ej, apply=1, translate=1, rotate=1, scale=1)
 
 sjl = cmds.spaceLocator(n=f'{sels[0]}_ik_loc')[0]
 group = cmds.group(sjl, n=f'{sels[0]}_root_ik_grp')
@@ -44,7 +52,43 @@ for p in zip([sels[2], sels[1]], [ikhl, polel]):
         cmds.xform(p[1], ws=1, t=cmds.xform(p[0], q=1, ws=1, t=1), ro=cmds.xform(p[0], q=1, ws=1, ro=1))
         cmds.setKeyframe(p[1])
 
+
+
+
+cmds.currentTime(int(cmds.playbackOptions(q=1, min=1)))
+
+#for p in zip(locs, sels):
+for p in zip([sj, mj], [sels[0], sels[1]]):
+    """
+    skipTrans = []
+    if cmds.getAttr(p[1] + '.tx', l=1):
+        skipTrans.append('x')
+    if cmds.getAttr(p[1] + '.ty', l=1):
+        skipTrans.append('y')
+    if cmds.getAttr(p[1] + '.tz', l=1):
+        skipTrans.append('z')
+    """
+
+    skipRot = []
+    if cmds.getAttr(p[1] + '.rx', l=1):
+        skipRot.append('x')
+    if cmds.getAttr(p[1] + '.ry', l=1):
+        skipRot.append('y')
+    if cmds.getAttr(p[1] + '.rz', l=1):
+        skipRot.append('z')
+
+    #cmds.parentConstraint(p[0], p[1], st=skipTrans, sr=skipRot, mo=0)
+    cmds.parentConstraint(p[0], p[1], st=['x', 'y', 'z'], sr=skipRot, mo=1)
+
+
+
+
+"""
 for p in zip([sj, mj], [sels[0], sels[1]]):
     cmds.parentConstraint(p[0], p[1], st=['x', 'y', 'z'], mo=1)
+"""
 
-cmds.orientConstraint(ikhl, sels[2], mo=0)
+cmds.orientConstraint(ikhl, sels[2], mo=1)
+
+cmds.select([ikhl, polel])
+
