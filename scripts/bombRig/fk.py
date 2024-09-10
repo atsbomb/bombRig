55edbc517846e0288c0f59158d4558627752ba36
atsbomb <atsbomb@gmail.com>
2024-09-10 17:00:11 +0800

    making fk and ik into function

--- scripts/bombRig/fk.py
+++ scripts/bombRig/fk.py
@@ -3,51 +3,53 @@
 
 import maya.cmds as cmds
 
-sels = cmds.ls(sl=1)
-parent = sels.pop(0)
-locs = []
+def run():
 
-for sel in sels:
-    loc = cmds.spaceLocator(n=f'{sel}_fk_loc')[0]
-    cmds.xform(loc, ws=1, t=cmds.xform(sel, q=1, ws=1, t=1), ro=cmds.xform(sel, q=1, ws=1, ro=1))
-    locs.append(loc)
+    sels = cmds.ls(sl=1)
+    parent = sels.pop(0)
+    locs = []
 
-for i in range(1, len(locs)):
-    cmds.parent(locs[i], locs[i-1])
+    for sel in sels:
+        loc = cmds.spaceLocator(n=f'{sel}_fk_loc')[0]
+        cmds.xform(loc, ws=1, t=cmds.xform(sel, q=1, ws=1, t=1), ro=cmds.xform(sel, q=1, ws=1, ro=1))
+        locs.append(loc)
 
-group = cmds.group(locs[0], n=f'{sels[0]}_fk_loc_grp')
-cmds.parentConstraint(parent, group)
+    for i in range(1, len(locs)):
+        cmds.parent(locs[i], locs[i-1])
 
-for loc in locs:
-    currentMatrix = cmds.xform(loc, q=1, matrix=1)
-    cmds.setAttr(f'{loc}.offsetParentMatrix', *currentMatrix, type='matrix')
-    cmds.setAttr(f'{loc}.t', 0, 0, 0)
-    cmds.setAttr(f'{loc}.r', 0, 0, 0)
-    cmds.setAttr(f'{loc}.s', 1, 1, 1)
+    group = cmds.group(locs[0], n=f'{sels[0]}_fk_loc_grp')
+    cmds.parentConstraint(parent, group)
 
-for p in zip(sels, locs):
-    for t in range(int(cmds.playbackOptions(q=1, min=1)), int(cmds.playbackOptions(q=1, max=1)) + 1):
-        cmds.currentTime(t)
-        cmds.xform(p[1], ws=1, t=cmds.xform(p[0], q=1, ws=1, t=1), ro=cmds.xform(p[0], q=1, ws=1, ro=1))
-        cmds.setKeyframe(p[1])
+    for loc in locs:
+        currentMatrix = cmds.xform(loc, q=1, matrix=1)
+        cmds.setAttr(f'{loc}.offsetParentMatrix', *currentMatrix, type='matrix')
+        cmds.setAttr(f'{loc}.t', 0, 0, 0)
+        cmds.setAttr(f'{loc}.r', 0, 0, 0)
+        cmds.setAttr(f'{loc}.s', 1, 1, 1)
 
-for p in zip(locs, sels):
-    skipTrans = []
-    if cmds.getAttr(p[1] + '.tx', l=1):
-        skipTrans.append('x')
-    if cmds.getAttr(p[1] + '.ty', l=1):
-        skipTrans.append('y')
-    if cmds.getAttr(p[1] + '.tz', l=1):
-        skipTrans.append('z')
+    for p in zip(sels, locs):
+        for t in range(int(cmds.playbackOptions(q=1, min=1)), int(cmds.playbackOptions(q=1, max=1)) + 1):
+            cmds.currentTime(t)
+            cmds.xform(p[1], ws=1, t=cmds.xform(p[0], q=1, ws=1, t=1), ro=cmds.xform(p[0], q=1, ws=1, ro=1))
+            cmds.setKeyframe(p[1])
 
-    skipRot = []
-    if cmds.getAttr(p[1] + '.rx', l=1):
-        skipRot.append('x')
-    if cmds.getAttr(p[1] + '.ry', l=1):
-        skipRot.append('y')
-    if cmds.getAttr(p[1] + '.rz', l=1):
-        skipRot.append('z')
+    for p in zip(locs, sels):
+        skipTrans = []
+        if cmds.getAttr(p[1] + '.tx', l=1):
+            skipTrans.append('x')
+        if cmds.getAttr(p[1] + '.ty', l=1):
+            skipTrans.append('y')
+        if cmds.getAttr(p[1] + '.tz', l=1):
+            skipTrans.append('z')
 
-    cmds.parentConstraint(p[0], p[1], st=skipTrans, sr=skipRot, mo=0)
+        skipRot = []
+        if cmds.getAttr(p[1] + '.rx', l=1):
+            skipRot.append('x')
+        if cmds.getAttr(p[1] + '.ry', l=1):
+            skipRot.append('y')
+        if cmds.getAttr(p[1] + '.rz', l=1):
+            skipRot.append('z')
 
-cmds.select(locs)
+        cmds.parentConstraint(p[0], p[1], st=skipTrans, sr=skipRot, mo=0)
+
+    cmds.select(locs)
--- scripts/bombRig/ik.py
+++ scripts/bombRig/ik.py
@@ -4,73 +4,63 @@
 
 import maya.cmds as cmds
 
-sels = cmds.ls(sl=1)
-all = sels.pop(0)
-parent = sels.pop(0)
+def run():
 
-cmds.select(cl=1)
-sj = cmds.joint(n=f'{sels[0]}_ik_jnt')
-cmds.matchTransform(sj, sels[0], position=1, rotation=1)
-cmds.makeIdentity(sj, apply=1, translate=1, rotate=1, scale=1)
-cmds.select(sj)
-mj = cmds.joint(n=f'{sels[1]}_ik_jnt')
-cmds.matchTransform(mj, sels[1], position=1, rotation=1)
-cmds.makeIdentity(mj, apply=1, translate=1, rotate=1, scale=1)
-cmds.select(mj)
-ej = cmds.joint(n=f'{sels[2]}_ik_jnt')
-cmds.matchTransform(ej, sels[2], position=1, rotation=1)
-cmds.makeIdentity(ej, apply=1, translate=1, rotate=1, scale=1)
+    sels = cmds.ls(sl=1)
+    all = sels.pop(0)
+    parent = sels.pop(0)
 
-sjl = cmds.spaceLocator(n=f'{sels[0]}_ik_loc')[0]
-group = cmds.group(sjl, n=f'{sels[0]}_root_ik_grp')
-cmds.parentConstraint(parent, group, mo=0)
-cmds.delete(cmds.pointConstraint(sj, sjl, mo=0))
-cmds.pointConstraint(sjl, sj, mo=0)
+    cmds.select(cl=1)
+    sj = cmds.joint(n=f'{sels[0]}_ik_jnt')
+    cmds.matchTransform(sj, sels[0], position=1, rotation=1)
+    cmds.makeIdentity(sj, apply=1, translate=1, rotate=1, scale=1)
+    cmds.select(sj)
+    mj = cmds.joint(n=f'{sels[1]}_ik_jnt')
+    cmds.matchTransform(mj, sels[1], position=1, rotation=1)
+    cmds.makeIdentity(mj, apply=1, translate=1, rotate=1, scale=1)
+    cmds.select(mj)
+    ej = cmds.joint(n=f'{sels[2]}_ik_jnt')
+    cmds.matchTransform(ej, sels[2], position=1, rotation=1)
+    cmds.makeIdentity(ej, apply=1, translate=1, rotate=1, scale=1)
 
-ikh, ee = cmds.ikHandle(sj=sj, ee=ej)
+    sjl = cmds.spaceLocator(n=f'{sels[0]}_ik_loc')[0]
+    group = cmds.group(sjl, n=f'{sels[0]}_root_ik_grp')
+    cmds.parentConstraint(parent, group, mo=0)
+    cmds.delete(cmds.pointConstraint(sj, sjl, mo=0))
+    cmds.pointConstraint(sjl, sj, mo=0)
 
-ikhl = cmds.spaceLocator(n=f'{sels[2]}_ik_loc')[0]
-cmds.pointConstraint(ikhl, ikh)
-polel = cmds.spaceLocator(n=f'{sels[1]}_pole_loc')[0]
-cmds.poleVectorConstraint(polel, ikh)
+    ikh, ee = cmds.ikHandle(sj=sj, ee=ej)
 
-mainGroup = cmds.group(group, sj, ikh, ikhl, polel, n=f'{sels[2]}_ik_loc_grp')
-cmds.parentConstraint(all, mainGroup, mo=1)
+    ikhl = cmds.spaceLocator(n=f'{sels[2]}_ik_loc')[0]
+    cmds.pointConstraint(ikhl, ikh)
+    polel = cmds.spaceLocator(n=f'{sels[1]}_pole_loc')[0]
+    cmds.poleVectorConstraint(polel, ikh)
 
-# might want to skip zero'ing out the xform for IK, do more tests with real examples
-"""
-for p in zip([sels[2], sels[1]], [ikhl, polel]):
-    cmds.currentTime(cmds.playbackOptions(q=1, min=1))
-    cmds.xform(p[1], ws=1, t=cmds.xform(p[0], q=1, ws=1, t=1), ro=cmds.xform(p[0], q=1, ws=1, ro=1))
-    currentMatrix = cmds.xform(p[1], q=1, matrix=1)
-    cmds.setAttr(f'{p[1]}.offsetParentMatrix', *currentMatrix, type='matrix')
-    cmds.setAttr(f'{p[1]}.t', 0, 0, 0)
-    cmds.setAttr(f'{p[1]}.r', 0, 0, 0)
-    cmds.setAttr(f'{p[1]}.s', 1, 1, 1)
-"""
+    mainGroup = cmds.group(group, sj, ikh, ikhl, polel, n=f'{sels[2]}_ik_loc_grp')
+    cmds.parentConstraint(all, mainGroup, mo=1)
 
-for p in zip([sels[2], sels[1]], [ikhl, polel]):
-    for t in range(int(cmds.playbackOptions(q=1, min=1)), int(cmds.playbackOptions(q=1, max=1)) + 1):
-        cmds.currentTime(t)
-        cmds.xform(p[1], ws=1, t=cmds.xform(p[0], q=1, ws=1, t=1), ro=cmds.xform(p[0], q=1, ws=1, ro=1))
-        cmds.setKeyframe(p[1])
+    for p in zip([sels[2], sels[1]], [ikhl, polel]):
+        for t in range(int(cmds.playbackOptions(q=1, min=1)), int(cmds.playbackOptions(q=1, max=1)) + 1):
+            cmds.currentTime(t)
+            cmds.xform(p[1], ws=1, t=cmds.xform(p[0], q=1, ws=1, t=1), ro=cmds.xform(p[0], q=1, ws=1, ro=1))
+            cmds.setKeyframe(p[1])
 
-cmds.currentTime(int(cmds.playbackOptions(q=1, min=1)))
+    cmds.currentTime(int(cmds.playbackOptions(q=1, min=1)))
 
-#for p in zip(locs, sels):
-for p in zip([sj, mj], [sels[0], sels[1]]):
-    skipRot = []
-    if cmds.getAttr(p[1] + '.rx', l=1):
-        skipRot.append('x')
-    if cmds.getAttr(p[1] + '.ry', l=1):
-        skipRot.append('y')
-    if cmds.getAttr(p[1] + '.rz', l=1):
-        skipRot.append('z')
+    #for p in zip(locs, sels):
+    for p in zip([sj, mj], [sels[0], sels[1]]):
+        skipRot = []
+        if cmds.getAttr(p[1] + '.rx', l=1):
+            skipRot.append('x')
+        if cmds.getAttr(p[1] + '.ry', l=1):
+            skipRot.append('y')
+        if cmds.getAttr(p[1] + '.rz', l=1):
+            skipRot.append('z')
 
-    #cmds.parentConstraint(p[0], p[1], st=skipTrans, sr=skipRot, mo=0)
-    cmds.parentConstraint(p[0], p[1], st=['x', 'y', 'z'], sr=skipRot, mo=1)
+        #cmds.parentConstraint(p[0], p[1], st=skipTrans, sr=skipRot, mo=0)
+        cmds.parentConstraint(p[0], p[1], st=['x', 'y', 'z'], sr=skipRot, mo=1)
 
-cmds.orientConstraint(ikhl, sels[2], mo=1)
+    cmds.orientConstraint(ikhl, sels[2], mo=1)
 
-cmds.select([ikhl, polel])
+    cmds.select([ikhl, polel])
 
